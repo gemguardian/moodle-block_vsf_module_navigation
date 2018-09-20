4b9e8016281f8efe6ee5aa2564680b84e002d057
Luuk Verhoeven <luuk@ldesign.nl>
2018-09-20 14:39:02 +0200

    Init the original code

---
+++ lang/en/block_course_modulenavigation.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of The Course Module Navigation Block
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block_course_modulenavigation
+ * @copyright  2016 Digidago <contact@digidago.com>
+ * @author     Bas Brands
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['config_blocktitle'] = 'Block title';
+$string['config_blocktitle_default'] = 'Table of contents';
+$string['config_blocktitle_help'] = 'Leave this field empty to use the default block title. If you define a title here, it will be used instead of the default one.';
+$string['config_onesection'] = 'Show the current section only';
+$string['config_onesection_label'] = 'If enabled show one section only. If disabled show an accordion style menu';
+$string['course_modulenavigation:addinstance'] = 'Add a new course contents block';
+$string['notusingsections'] = 'This course format does not use sections.';
+$string['pluginname'] = 'Course Module Navigation';
+$string['toggleclickontitle'] = 'Clicking on the title';
+$string['toggleclickontitle_desc'] = "'Displays the menu' or 'Goes to that page'.";
+$string['toggleclickontitle_menu'] = 'Displays the menu';
+$string['toggleclickontitle_page'] = 'Goes to that page';
+$string['toggleshowlabels'] = 'Show labels';
+$string['toggleshowlabels_desc'] = 'Choose whether to display labels or not';
+$string['togglecollapse'] = 'Collapse tabs';
+$string['togglecollapse_desc'] = 'Choose whether to display all tabs not collapsed';
+$string['toggletitles'] = 'Show only titles';
+$string['toggletitles_desc'] = 'Choose whether to display only titles or titles and content';
+$string['modulepageonly'] = 'Show this block on module pages only';
+$string['modulepageonly_desc'] = 'When set to yes the block will only be shown on Modulepages not coursepages';
+$string['alertmodulepageonly'] = 'This block\'s global settings have been configured to show on modulepages only, but the setting the show on this page on module pages has not been configured correctly. Please update the preferences for this block to "Display on page types" => "Any Page"';
+$string['onesectionsimplified'] = 'Simplify One section view';
+$string['onesectionsimplified_desc'] = 'When the One section view is configured in the block preferences hide navigation arrows.';
+$string['circle'] = 'Circle';
+$string['check'] = 'Check';
+$string['completionchecktype'] = 'Icon type for completion checking';
+$string['completionchecktype_desc'] = 'Show this type of icon on completion';
+$string['completionchecklocation'] = 'Icon location for completion checking';
+$string['completionchecklocation_desc'] = 'Show checkes before or after the activity name';
+$string['left'] = 'Left';
+$string['right'] = 'Right';
---
+++ lang/nl/block_course_modulenavigation.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of The Course Module Navigation Block
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block_course_modulenavigation
+ * @copyright  2016 Digidago <contact@digidago.com>
+ * @author     Bas Brands
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['config_blocktitle'] = 'Blok titel';
+$string['config_blocktitle_default'] = 'Menu';
+$string['config_blocktitle_help'] = 'Laat dit veld leeg om de standaard blok titel te gebruiken. Plaats hier eventueel andere titel.';
+$string['config_onesection'] = 'Toon alleen de nu getoonde sectie';
+$string['config_onesection_label'] = 'wanneer aangevinkt zal maar één sectie getoond worden. Anders wordt een accordion menu getoond.';
+$string['course_modulenavigation:addinstance'] = 'Voeg nieuwe cursus content blok toe';
+$string['notusingsections'] = 'Dit cursusformat gebruikt geen secties.';
+$string['pluginname'] = 'Course Module Navigation';
+$string['toggleclickontitle'] = 'Klik op titel';
+$string['toggleclickontitle_desc'] = "'Toon menu' or 'Ga naar pagina'.";
+$string['toggleclickontitle_menu'] = 'Toon menu';
+$string['toggleclickontitle_page'] = 'Ga naar pagina';
+$string['toggleshowlabels'] = 'Toon labels';
+$string['toggleshowlabels_desc'] = 'Kies hier of je wel of geen labels wilt tonen';
+$string['togglecollapse'] = 'Uitklapbare tabs';
+$string['togglecollapse_desc'] = 'Kies hier of je de tabs wil laten uitklappen of niet';
+$string['toggletitles'] = 'Toon alleen titels';
+$string['toggletitles_desc'] = 'Kies of alleen titels wilt tonen of titels en content';
+$string['modulepageonly'] = 'Toon dit blok alleen op module pagina.';
+$string['modulepageonly_desc'] = 'Wanneer op ja, zal het blok alleen getoond worden op de module pagina\'s en niet de cursuspagina\'s';
+$string['alertmodulepageonly'] = 'De standaard instellingen van dit blok is tonen op module pagina\'s, maar de instellingen van het blok zelf staan nog niet goed. Pas de instellingen van het blok aan bij "Toon op paginatypes" => "Gelijk welke pagina"';
+$string['onesectionsimplified'] = 'Eenvoudige een sectie view';
+$string['onesectionsimplified_desc'] = 'When the One section view is configured in the block preferences hide navigation arrows.';
+$string['circle'] = 'Rondje';
+$string['check'] = 'Vinkje';
+$string['completionchecktype'] = 'Icoon type voor voltooingsvoorwaarde';
+$string['completionchecktype_desc'] = 'Toon dit type als activiteit/bron voltooid is';
+$string['completionchecklocation'] = 'Waar wil je de icoon voor voltooiingsvoorwaarden tonen';
+$string['completionchecklocation_desc'] = 'Toon checks voor of achter de activiteitsnaam';
+$string['left'] = 'Links';
+$string['right'] = 'Rechts';
